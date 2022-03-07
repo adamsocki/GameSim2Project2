050c128d56193ec494b93175260f467220f3b87e
LarryDavid411 <65431559+LarryDavid411@users.noreply.github.com>
2022-03-06 20:55:22 -0600

    v1

---
+++ GameSim2Project2/Assets/CameraController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject camera;
+
+    public GameObject player;
+    public Vector3 cameraPositionShift;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPosition = player.transform.position;
+        Vector3 cameraPosition = new Vector3(playerPosition.x, playerPosition.y , playerPosition.z);
+        cameraPosition += cameraPositionShift;
+        camera.GetComponent<Transform>().position = cameraPosition;
+
+    }
+}
--- GameSim2Project2/Assets/PlayerMover.cs
+++ GameSim2Project2/Assets/PlayerMover.cs
@@ -1,18 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
+    private CharacterController controller;
+
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+          
+        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+
+        Vector3 dir = new Vector3(x, 0, z);
+
+        // if (dir.magnitude >= 0.0f)
+        // {
+        //     if (prevY == 0.0 && prevX == 0.0)
+        //     {
+        //         moveStartTime = Time.time;
+        //
+        //     }
+        //     
+        // }
+
+       // float timeSinceLastMove = Time.time - moveStartTime;
+
+       // float t = timeSinceLastMove / accelFactor;
+
+        //float speed = Mathf.Lerp(0.0f, maxSpeed, t);
+
+        controller.Move(dir * speed * Time.deltaTime);
         
+        //Debug.Log(speed);
+
+        //prevX = x;
+        //prevY = y; 
     }
 }
