9a7fde077711ba97ec95d35f438560df8d0d2ea2
LarryDavid411 <65431559+LarryDavid411@users.noreply.github.com>
2022-03-17 13:19:54 -0500

    workin on main menu

--- GameSim2Project2/Assets/EnemyAI_A.cs
+++ GameSim2Project2/Assets/EnemyAI_A.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class EnemyAI_A : MonoBehaviour
 {
+    public bool enemyOnGround;
+    public enum AIState
+    {
+        Walking,
+        LookingForPlayer,
+        LookingAtPlayer,
+        ShootingAtPlayer,
+        AttackingPlayer,
+        Dead
+    }
+
+    public AIState enemyState;
+    //AIState enemyState = AIState.Walking;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyState = AIState.Walking;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        
-        
+        switch (enemyState)
+        {
+            case AIState.Walking:
+            {
+                
+            } break;
+            
+            case AIState.AttackingPlayer:
+            {
+                
+            } break;
+        }
     }
 }
--- GameSim2Project2/Assets/LevelTemplateController.cs
+++ GameSim2Project2/Assets/LevelTemplateController.cs
@@ -5,29 +5,25 @@ using UnityEngine;
 public class LevelTemplateController : MonoBehaviour
 {
     public GameObject levelManager;
+    public GameObject ground;
 
-    public GameObject barrierSector1;
-    
     public GameObject[] barrierSectors = new GameObject[4];
 
     private Vector3 barrierPosition;
-
     private Vector3 barrierSize;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
         float currentLevel = levelManager.GetComponent<LevelController>().currentLevel;
         float currentLevelZFactor = (100 * currentLevel);
         Vector3 positionForNewLevel = new Vector3(0, 1, currentLevelZFactor);
 
         // enemy creation
         int numberOfEnemies = Random.Range(0, 4);
-
         for (int i = 0; i < numberOfEnemies; i++)
         {
-                
-            
+
         }
         // Gme
         gameObject.transform.position = positionForNewLevel;
@@ -42,7 +38,7 @@ public class LevelTemplateController : MonoBehaviour
             // barrier placement
             barrierPosition.z = currentLevelZFactor - (80f) + (20f * i);
             barrierPosition.x = Random.Range(-20, 30);
-            //barrierPosition.y = ()
+            barrierPosition.y = ground.transform.localPosition.y + (barrierSize.y / 2) + 1.5f;
             Debug.Log(barrierPosition.x);
             barrierSectors[i].transform.position = barrierPosition;
         }
